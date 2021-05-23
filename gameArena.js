ffd1ba97add3ad1669afe5d835678dfe0c5412b2
Sukhbeer Mangat <mangatsukhbeer@gmail.com>
2021-05-23 06:28:18 -0400

    Added blocks.js and Block code

--- gameArena.js
+++ gameArena.js
@@ -34,3 +34,51 @@ function drawSquare(x,y,color){
 // ------------------------
 
 //sukh
+
+const BLOCKS = [
+    [Z,"red"],
+    [S,"green"],
+    [T,"yellow"],
+    [O,"blue"],
+    [L,"purple"],
+    [I,"cyan"],
+    [J,"orange"]
+];
+
+function randomBlock(){
+    let r = randomB = Math.floor(Math.random() * BLOCKS.length)
+    return new Block( BLOCKS[r][0], BLOCKS[r][1]);
+}
+
+let b = new Block(BLOCKS[0][0],BLOCKS[0][1]);
+
+function Block(tetromino,color){
+    this.tetromino = tetromino;
+    this.color = color;
+
+    this.tetrnominoNumber = 0;
+    this.activeTetromino = this.tetromino[this.tetrnominoNumber];
+
+    this.x = 5;
+    this.y = 5;
+}
+
+Block.prototype.fill = function(color){
+    for( r = 0; r < this.activeTetromino.length; r++){
+        for( c = 0; c < this.activeTetromino.length;c++){
+            if(this.activeTetromino[r][c]){
+                drawSquare(this.x + c, this.y + r, color);
+            }
+        }
+    }
+}
+
+Block.prototype.draw = function(){
+    this.fill(this.color);
+}
+
+Block.prototype.unDraw = function(){
+    this.fill(VACANT);
+}
+
+b.draw;
